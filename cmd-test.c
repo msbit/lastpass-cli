36c82bc80a1604e2f07b9517c050a93acc6b3605
Tom Sullivan <tom@msbit.com.au>
2019-03-15 14:58:52 +1100

    cmd-test

--- cmd-test.c
+++ cmd-test.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
 #include "config.h"
 
 int cmd_test()
 {
-	printf("config_path(\"\"):                                 %s\n", config_path(""));
-	printf("config_path(\"upload-queue\"):                     %s\n", config_path("upload-queue"));
-	printf("config_path(\"upload-queue/15514988830000.lock\"): %s\n", config_path("upload-queue/15514988830000.lock"));
+	printf("%s\n", config_path("env"));
+	printf("%s\n", config_path("alias.ls"));
+	printf("%s\n", config_path("plaintext_key"));
+	printf("%s\n", config_path("agent.sock"));
+	printf("%s\n", config_path("iterations"));
+	printf("%s\n", config_path("username"));
+	printf("%s\n", config_path("verify"));
+	printf("%s\n", config_path("session_uid"));
+	printf("%s\n", config_path("session_sessionid"));
+	printf("%s\n", config_path("session_token"));
+	printf("%s\n", config_path("session_server"));
+	printf("%s\n", config_path("session_privatekey"));
+	printf("%s\n", config_path("blob"));
+	printf("%s\n", config_path("upload-queue/15514988830000.lock"));
 	return 0;
 }
